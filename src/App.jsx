37a81a06cb06e2a446c93964536ffa3632aea3af
alexmil-sk <alexmlrdv@gmail.com>
2022-08-31 12:07:46 +0200

    Context was deleted.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
-import React, {useState, createContext} from "react";
+import React from "react";
 import {Routes, Route} from "react-router-dom";
 import AppHeader from "./components/AppHeader/AppHeader.jsx";
 import Home from "./pages/Home/Home.jsx";
 import NotFound from "./pages/NotFound/NotFound.jsx";
 import Cart from "./pages/Cart/Cart.jsx";
 
-export const ContextSearchField = createContext({});
-
 
 function App() {
-  
-  const [searchField, setSearchField] = useState('');
-  
-  return (
+    return (
     <div className="wrapper">
-      <ContextSearchField.Provider value={{searchField, setSearchField}}>
         <AppHeader/>
         
         <Routes>
@@ -22,7 +16,6 @@ function App() {
           <Route path="cart" element={<Cart/>}/>
           <Route path="*" element={<NotFound/>}/>
         </Routes>
-      </ContextSearchField.Provider>
     </div>
   )
 }
